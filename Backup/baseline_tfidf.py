3f8b411fe420113a8e2e1d0dea0dac3ccd8d44b6
angelosbc <angelosbc@hotmail.com>
2026-08-18 00:44:09 -0300

    Delete Backup directory

--- Backup/baseline_tfidf.py
+++
@@ -1,7 +0,0 @@
-# Pipeline Baseline TF-IDF + Logistic Regression
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
-
-print("Script de Baseline pronto para execucao.")
